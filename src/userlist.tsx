3b8192338f192597e79d71d54d0ffac6358b1ec4
Dawit Fissha <dawitfissha1@gmail.com>
2022-03-18 12:08:11 +0300

    LastLogin field is added and now the grid is sortable by all fields

--- src/userlist.tsx
+++ src/userlist.tsx
@@ -1,19 +1,36 @@
 import * as React from 'react'
-import { Grid, GridColumn as Column } from "@progress/kendo-react-grid";
-import users from './user.json'
- export const UserList = ()=>{
+import { Grid,
+    GridColumn as Column,
+    GridDataStateChangeEvent,
+    GridCellProps,} from "@progress/kendo-react-grid";
+    import { process, State } from "@progress/kendo-data-query";
+import {users} from './user'
+const initialDataState: State = {
+    sort: [{ field: "code", dir: "asc" }],
+  };
+export const UserList = ()=>{
+    const [dataState,setDataState] = React.useState<State>(initialDataState)
     return(
         <div>
             <h2>User list</h2>
             <Grid
-    style={{
-        height: "400px",
-    }}
-    data={users}
+                style={{
+                    height: "400px",
+                }}
+                data={process(users,dataState)}
+                sortable={true}
+                {...dataState}
+                onDataStateChange = {
+                    (e:GridDataStateChangeEvent)=>{
+                        setDataState(e.dataState);
+                    }
+                }
+                
     >
     <Column field="UserName" title="User Name" width="250px" />   
     <Column field="firstName" title="First Name" width="250px" />
     <Column field="lastName" title="Last Name" />
+    <Column field="LastLogin" title="last login" editor="date" format="{0:d}" />
     <Column field="enabled" title="Enabled" />
     </Grid>
         </div>
